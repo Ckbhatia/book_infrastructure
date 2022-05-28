4f557f71855c93d09bfb16ee852d665e3ca9b8b8
Ckbhatia <chetansain86@gmail.com>
2022-05-28 22:16:26 +0530

    feat: add infrastructure stack

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,16 +1,30 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, aws_s3 as s3, aws_cloudfront as cloudfront } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    
+    const bucket = new s3.Bucket(this, 'BooksApp', {
+      bucketName: 'books-app-bucket',
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: new s3.BlockPublicAccess({ restrictPublicBuckets: false })
+    });
 
-    // The code that defines your stack goes here
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, 'OAI');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'MyDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: cloudFrontOAI,
+          },
+          behaviors: [{ isDefaultBehavior: true }]
+        }
+      ]
+    })
+
+    bucket.grantRead(cloudFrontOAI.grantPrincipal);
   }
 }
